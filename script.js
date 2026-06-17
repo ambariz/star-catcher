5fc861d47004786af9eae1e0b2622feab39dc6df
Ambarish S <imrealambarish@gmail.com>
2026-06-17 14:26:52 +0000

    completed game

--- script.js
+++ script.js
@@ -20,55 +20,60 @@ resizeCanvas();
 
 window.addEventListener("resize", resizeCanvas)
 
+const musicNewHighScore  = new Audio("assets/8_bit_level_complete.wav")
+const musicGameStart = new Audio("assets/brass_level_start.wav")
+const musicText = new Audio("assets/Text 1.wav")
+const musicLevel1 = new Audio("assets/match_synth_1.wav")
+const musicLevel2 = new Audio("assets/match_synth_2.wav")
+const musicLevel3 = new Audio("assets/match_synth_3.wav")
+const musicLevel4 = new Audio("assets/match_synth_4.wav")
+const musicLevel5 = new Audio("assets/match_synth_5.wav")
+const musicLevel6 = new Audio("assets/match_synth_6.wav")
+const musicLevel7 = new Audio("assets/match_synth_7.wav")
+const musicLevel8 = new Audio("assets/match_synth_8.wav")
+const musicLevel9 = new Audio("assets/match_synth_9.wav")
+const musicLevel10 = new Audio("assets/match_synth_10.wav")
 
+const cometImage = new Image();
+cometImage.src = "assets/comet.png";
+
+const playerImage = new Image();
+playerImage.src = "assets/player.png";
+
+const shieldImage = new Image();
+shieldImage.src = "assets/shield.png";
 
 const items = [];
 let score = 0;
 let lives = 3; 
 let gameOver = false;
 let highScore = localStorage.getItem("highScore") || 0;
+let newHighScoreReached = false;
+
+let currentLevel = 1;
+
 
 let gameStarted = false
+let bigHands = false;
+let powerUpTimer = 0;
+let shieldActive = false;
 
-
+let rgbFlash = false;
+let rgbFlashTimer = 0;
 
 function drawPlayer() {
-    ctx.fillStyle = "#FFD166";
-    ctx.beginPath();
-    ctx.arc(
-        player.x + player.width / 2,
-        player.y + player.height / 2,
-        20,
-        0,
-        Math.PI * 2
+    if (shieldActive)
+    {
+    ctx.drawImage(
+        shieldImage,
+        player.x - 20,
+        player.y - 20,
+        player.width + 40,
+        player.height + 40
     );
-    ctx.fill();
+    }
+    ctx.drawImage( playerImage, player.x, player.y, player.width, player.height);
 
-    ctx.beginPath();
-    ctx.moveTo(player.x + 8, player.y + 10);
-    ctx.lineTo(player.x + 15, player.y - 8);
-    ctx.lineTo(player.x + 22, player.y + 10);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.moveTo(player.x + 32, player.y + 10);
-    ctx.lineTo(player.x + 25, player.y - 8);
-    ctx.lineTo(player.x + 18, player.y + 10);
-    ctx.fill();
-
-    ctx.fillStyle = "#000";
-
-    ctx.beginPath();
-    ctx.arc(player.x + 14, player.y + 18, 2, 0, Math.PI * 2);
-    ctx.fill();
-
-    ctx.beginPath(); 
-    ctx.arc(player.x + 26, player.y + 18, 2, 0, Math.PI * 2);
-    ctx.fill();
-
-    ctx.beginPath();
-    ctx.arc(player.x + 20, player.y + 24, 5, 0, Math.PI);
-    ctx.stroke();
 }
 
 function drawStar(x, y, size) {
@@ -129,9 +134,23 @@ function drawBadStar(x, y, size) {
 function drawItems() {
     items.forEach(item => {
 
-        if (item.isBad) {
+        if (item.isPowerUp) {
+            ctx.drawImage(cometImage, item.x - 40, item.y - 40, 80, 80);
+        }
+
+        else if (item.isBad) {
             drawBadStar(item.x, item.y, 12);
-        } else {
+        } 
+        else if (item.isShield) {
+            ctx.drawImage(
+                shieldImage,
+                item.x - 50,
+                item.y - 50,
+                40,
+                40
+            )
+        }
+        else {
             drawStar(item.x, item.y, 12);
         }
 
@@ -175,12 +194,39 @@ function drawBackground()
         );
         ctx.fill();
     }
+    if (rgbFlash) {
+        const colours = ["#ff0000", "#00ff00", "#0000ff"];
+        ctx.fillStyle = colours[Math.floor(rgbFlashTimer / 5) % 3];
+        ctx.fillRect(0,0, canvas.width, canvas.height);
+        rgbFlashTimer--;
+        if (rgbFlashTimer <= 0) {
+            rgbFlash = false;
+        }
+        return;
+    }
 }
 
 function updatePlayer() {
     player.x += player.dx;
     if (player.x < 0) player.x = 0;
     if (player.x + player.width > canvas.width) player.x = canvas.width - player.width;
+    if (bigHands) {
+        powerUpTimer--;
+        if (powerUpTimer <= 0) {
+            bigHands = false;
+            player.width = 60;
+            player.height = 60;
+        }
+    }
+}
+
+function drawPowerUpStatus() {
+    if (bigHands) {
+        ctx.fillStyle = "#00ffff";
+        ctx.font = "bold 20px Arial";
+
+        ctx.fillText("BIG HANDS ACTIVE!!", canvas.width / 2 - 90, 60);
+    }
 }
 
 function drawWelcome() {
@@ -210,18 +256,62 @@ function updateItems() {
             item.y < player.y + player.height
         ) {
             items.splice(index, 1);
-
-            if (item.isBad) {
-                lives--;
+            
+            if (item.isPowerUp) {
+                bigHands = true;
+                powerUpTimer = 500;
+                player.width = 180;
+                player.height = 70;
+            }
+            else if (item.isShield) {
+                shieldActive = true;
+            }
+            else if (item.isBad) {
+                if (shieldActive) {
+                    shieldActive = false;
+                }
+                else {
+                    lives--;
+                }
             } 
+
             else {
                 score++;
+
+                const newLevel = Math.floor(score / 10) + 1;
+
+                if (newLevel > currentLevel) {
+                    currentLevel = newLevel;
+
+                    switch (currentLevel) {
+                        case 1: musicLevel1.play(); break;
+                        case 2: musicLevel1.play(); break;
+                        case 3: musicLevel1.play(); break;
+                        case 4: musicLevel1.play(); break;
+                        case 5: musicLevel1.play(); break;
+                        case 6: musicLevel1.play(); break;
+                        case 7: musicLevel1.play(); break;
+                        case 8: musicLevel1.play(); break;
+                        case 9: musicLevel1.play(); break;
+                        case 10: musicLevel1.play(); break;
+                    }
+                }
 
                 if (score > highScore)
                 {
                     highScore = score;
 
                     localStorage.setItem("highScore", highScore)
+                    
+                    if (!newHighScoreReached) {
+                        musicNewHighScore.currentTime = 0;
+                        musicNewHighScore.play();
+
+                        rgbFlash = true;
+                        rgbFlashTimer = 120;
+                        newHighScoreReached = true;
+    
+                    }
                 };
             }
         }
@@ -240,12 +330,17 @@ function createItem()
 {
     const badStarChance = Math.min(0.20 + (score * 0.015), 0.80);
     const isBad = Math.random() < badStarChance;
+    const isPowerUp = Math.random() < 0.03;
+    const isShield = !isPowerUp && Math.random() < 0.07;
+
     const item = {
         x: Math.random() * (canvas.width - 40) + 20,
         y: -20,
         radius: 20,
         dy: Math.random() * 2 + 2 + (score * 0.1),
-        isBad: isBad
+        isPowerUp,
+        isShield,
+        isBad: !isPowerUp && !isShield && isBad
     };
 
     items.push(item);
@@ -288,6 +383,7 @@ function gameLoop() {
     drawScore();
     drawLevel();
     drawLives();
+    drawPowerUpStatus();
     
     requestAnimationFrame(gameLoop);
 
@@ -308,13 +404,20 @@ document.addEventListener("keydown", (e) => {
 
         score = 0;
         lives = 3;
+        currentLevel = 1;
         gameOver = false;
+        newHighScoreReached = false;
+        bigHands = false;
+        powerUpTimer = 0;
+        shieldActive = false;
         items.length = 0;
 
         gameLoop();
     }
 
     if (e.code === "Space" && !gameStarted) {
+        musicGameStart.play();
+        musicText.play();
         gameStarted = true;
     }
 });
